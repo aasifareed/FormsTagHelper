624f8f0b7b3fe97f20429a7dfbebd7ffc2e29edf
Muhammad Asif <muhammad.asif@estratcom.com>
2021-02-06 09:17:57 +0500

    Add project files.

---
+++ FormsTagHelper/Controllers/HomeController.cs
@@ -0,0 +1,64 @@
+﻿using FormsTagHelper.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsTagHelper.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            return View(registerViewModel);
+        }
+
+        [HttpPost]
+        public IActionResult Index(RegisterViewModel registerViewModel)
+        {
+            if(!ModelState.IsValid)
+            return View(registerViewModel);
+
+            return View();
+        }
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+            // return View("AboutBoldOnly");
+            return View();
+        }
+
+        public IActionResult Contact(int id = 0)
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            string view = "Contact";
+
+            switch (id)
+            {
+                case 1:
+                    view = "ContactCopy";
+                    break;
+
+                case 2:
+                    view = "ContactVoid";
+                    break;
+
+                case 0:
+                default:
+                    view = "Contact";
+                    break;
+            }
+            return View(view);
+        }
+
+        public IActionResult Error()
+        {
+            return View();
+        }
+    }
+}
---
+++ FormsTagHelper/Models/RegisterViewModel.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FormsTagHelper.Models
+{
+    public class RegisterViewModel
+    {
+
+
+
+        [Required]
+        [EmailAddress]
+        [Display(Name = "Email Address")]
+        public string Email { get; set; }
+
+        [Required]
+        [DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required]
+        public string Country { get; set; }
+
+        public List<SelectListItem> Countries { get; } = new List<SelectListItem>
+        {
+            new SelectListItem(),
+            new SelectListItem { Value = "MX", Text = "Mexico" },
+            new SelectListItem { Value = "CA", Text = "Canada" },
+            new SelectListItem { Value = "US", Text = "USA"  },
+        };
+
+        [Required]
+        public IEnumerable<string> CountryCode { get; set; }
+
+        public List<SelectListItem> CountriesCodes { get; } = new List<SelectListItem>
+        {
+            new SelectListItem { Value = "MX", Text = "Mexico" },
+            new SelectListItem { Value = "CA", Text = "Canada" },
+            new SelectListItem { Value = "US", Text = "USA"    },
+            new SelectListItem { Value = "FR", Text = "France" },
+            new SelectListItem { Value = "ES", Text = "Spain"  },
+            new SelectListItem { Value = "DE", Text = "Germany"}
+         };
+
+    }
+}
